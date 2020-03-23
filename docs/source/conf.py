2d4fe1513f14c75776d9a9370340bba97f39da4c
pyMatJ <62421390+pyMatJ@users.noreply.github.com>
2020-03-23 15:37:10 +0100

    Try to solve RTD conflict

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('../../'))
-#sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')),'GTM','examples'))
 
 
 # -- Project information -----------------------------------------------------
@@ -46,7 +44,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
+## RTD expected the master file to be contents.rst
+master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
