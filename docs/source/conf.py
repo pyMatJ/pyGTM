75ef7f79f16f0f165d3fcb1651656325957b5c3f
pyMatJ <62421390+pyMatJ@users.noreply.github.com>
2020-03-27 15:41:22 +0100

    Solving autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 import mock
 sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('./../../GTM/'))
 
 
 # -- Project information -----------------------------------------------------
